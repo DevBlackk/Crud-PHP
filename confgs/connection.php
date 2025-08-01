aab9f63f86fb86aa1f1fd52a032646766efe6bfb
Davidson William <127349063+DevBlackk@users.noreply.github.com>
2025-07-31 22:03:04 -0300

    Delete confgs/connection.php

--- confgs/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('HOST', 'ep-spring-mud-aciy5d8x-pooler.sa-east-1.aws.neon.tech');
-define('USUARIO', 'neondb_owner');
-define('SENHA', 'npg_4Jn7PpTWBsAR');
-define('DB', 'neondb');
-
-$connection = pg_connect('postgresql://neondb_owner:npg_4Jn7PpTWBsAR@ep-spring-mud-aciy5d8x-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require') or die ("Falha na conexão!");
-?>
